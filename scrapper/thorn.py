953405d026c9114aeb27f7fd055f2ed2c50b7541
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-10-03 09:35:07 -0400

    Update thorn.py

--- scrapper/thorn.py
+++ scrapper/thorn.py
@@ -1,114 +1,152 @@
-# thorn.py
+#thorn.py
+
 
 import requests
 from bs4 import BeautifulSoup
 import json
+import os
+from concurrent.futures import ThreadPoolExecutor
+from cachetools import TTLCache
+import sqlite3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pdfplumber
+from PIL import Image
+import pytesseract
+import cv2
+import numpy as np
 
-class Thorn:
+# Config
+CACHE_TTL = 60 # 1 minute
+MAX_THREADS = 10
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+]  
+
+class Scraper:
+
     def __init__(self):
         self.session = requests.Session()
+        self.cache = TTLCache(maxsize=100, ttl=CACHE_TTL)
+        self.user_agents = USER_AGENTS
+
+    def scrape(self, url):
+        try:
+            response = self.session.get(url, headers={
+                'User-Agent': random.choice(self.user_agents)
+            })
+            content = response.text
+        except Exception as e:
+            print(e)
+            return
+        
+        key = hashlib.sha1(url.encode('utf8')).hexdigest()
+        self.cache[key] = content
+        return content
 
     def scrape_html(self, url):
-        try:
-            response = self.session.get(url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # Extract and return HTML data
-            return soup.prettify()
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return None
+        html = self.scrape(url)
+        if html:
+            return BeautifulSoup(html, 'html.parser')
 
     def scrape_json(self, url):
-        try:
-            response = self.session.get(url)
-            response.raise_for_status()
-            json_data = response.json()
-            return json_data
-        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
-            print(f"Error: {e}")
-            return None
+        json_text = self.scrape(url)
+        if json_text:
+            return json.loads(json_text)
 
-    def scrape_css(self, url):
-        try:
-            response = self.session.get(url)
-            response.raise_for_status()
-            # Extract and return CSS content
-            return response.text
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return None
+    def scrape_images(self, url, dest_folder):
+        soup = self.scrape_html(url)
+        img_tags = soup.find_all('img')
 
-    def scrape_python(self, code_url):
-        try:
-            response = self.session.get(code_url)
-            response.raise_for_status()
-            # Extract and return Python code content
-            return response.text
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
-            return None
+        os.makedirs(dest_folder, exist_ok=True)
 
-    def scrape_user_choice(self):
-        url = input("Enter the URL you want to scrape: ")
-        content_type = input("Enter the content type to scrape (html/json/css/python): ").lower()
+        for tag in img_tags:
+            img_url = tag['src']
+            img_data = self.scrape(img_url)
 
-        if content_type == "html":
-            result = self.scrape_html(url)
-        elif content_type == "json":
-            result = self.scrape_json(url)
-        elif content_type == "css":
-            result = self.scrape_css(url)
-        elif content_type == "python":
-            result = self.scrape_python(url)
-        else:
-            print("Invalid content type. Supported types: html/json/css/python")
-            return
+            filename = os.path.join(dest_folder, os.path.basename(img_url))
 
-        if result is not None:
-            print(result)
+            with open(filename, 'wb') as f:
+                f.write(img_data)
 
-class ImageThorn:
-    def __init__(self):
-        self.session = requests.Session()
+        print(f"Images saved to {dest_folder}")
 
-    def scrape_images(self, url, output_folder="./images"):
-        try:
-            response = self.session.get(url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.text, 'html.parser')
+    def scrape_json_api(self, url, params=None):
+        response = requests.get(url, params=params)
+        data = response.json()
 
-            # Create the output folder if it doesn't exist
-            os.makedirs(output_folder, exist_ok=True)
+        if 'next' in response.links:
+            next_url = response.links['next']['url']
+            data.extend(self.scrape_json_api(next_url))
 
-            # Find all image tags in the HTML
-            img_tags = soup.find_all('img')
+        return data
+    
+    def scrape_javascript(self, url):
+        driver = webdriver.Chrome()
+        driver.get(url)
 
-            for img_tag in img_tags:
-                img_url = img_tag.get('src')
-                if img_url:
-                    img_url = img_url.strip()
-                    img_name = os.path.basename(img_url)
+        html = driver.page_source
+        driver.close()
 
-                    # Construct the complete image URL
-                    if not img_url.startswith('http'):
-                        img_url = url + img_url
+        return html
 
-                    img_response = self.session.get(img_url)
+    def scrape_pdf(self, url):
+        pdf_data = self.scrape(url)
+        
+        with pdfplumber.open(io.BytesIO(pdf_data)) as pdf:
+            full_text = ''
+            for page in pdf.pages:
+                full_text += page.extract_text()
+            return full_text
 
-                    # Save the image to the output folder
-                    with open(os.path.join(output_folder, img_name), 'wb') as img_file:
-                        img_file.write(img_response.content)
+def scrape_graph(self, url):
 
-            print(f"Images scraped and saved to {output_folder}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error: {e}")
+  img_data = self.scrape(url)
 
-# Usage example
-if __name__ == "__main__":
-    thorn = Thorn()
-    image_thorn = ImageThorn()
+  img = Image.open(io.BytesIO(img_data))  
+  img = np.array(img)
+  
+  # Convert to grayscale
+  img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+  
+  # Apply threshold to make points white and background black
+  ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV) 
 
-    thorn.scrape_user_choice()  # Use the Thorn class to scrape other content
-    image_thorn.scrape_images("https://legends.dbz.space/characters/", output_folder="./images")
+  # Find contours in image
+  contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+  data_points = []
+
+  for c in contours:
+    # Extract coordinate of centroid of each contour
+    M = cv2.moments(c)
+    if M["m00"] != 0:
+      cX = int(M["m10"] / M["m00"])
+      cY = int(M["m01"] / M["m00"])
+      data_points.append((cX, cY))
+
+  # Sort data points by x coordinate  
+  data_points = sorted(data_points, key=lambda x: x[0]) 
+
+  return data_points
+
+scraper = Scraper()
+
+# Scrape a normal HTML page
+soup = scraper.scrape_html('http://example.com')
+
+# Scrape a JSON API
+data = scraper.scrape_json('http://api.example.com/data')
+
+# Scrape and download images 
+scraper.scrape_images('http://example.com/gallery', 'scraped_images')
+
+# Scrape JavaScript rendered page
+html = scraper.scrape_javascript('http://example.com')
+
+# Extract text from a PDF file
+text = scraper.scrape_pdf('http://example.com/doc.pdf')
+
+# Extract data points from a chart image
+data = scraper.scrape_graph('http://example.com/chart.png')
